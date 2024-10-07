6641c185a8892ee106c5cd38cc1054d7cbb9af49
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2024-10-08 00:44:55 +0200

    improve: diverse

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+// import { cleanup } from '@testing-library/react';
+
+export const dateString = "2024-10-06T19:40:55.221Z";
+export const dateValue = new Date(dateString);
+export const locale = "en-EN";
+export const formatDateTimeOptions: Intl.DateTimeFormatOptions = { dateStyle: "medium", timeStyle: "medium" };
+
+// afterEach(cleanup);
