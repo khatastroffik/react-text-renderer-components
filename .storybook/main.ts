e5c2507a728fe9c83861c41ad8dad90229b725ff
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2024-11-02 07:22:20 +0100

    docs: add introduction page in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,10 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/components/introduction.mdx", "../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
-    // "@storybook/addon-links",
+    "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
   framework: {
