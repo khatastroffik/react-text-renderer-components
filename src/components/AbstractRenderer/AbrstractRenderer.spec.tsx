fafb4f72aa790960f4f85de32f33bf6aa1069be0
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2024-12-31 08:35:07 +0100

    test: refine abstractrenderer tests, improve coverage

--- src/components/AbstractRenderer/AbrstractRenderer.spec.tsx
+++ src/components/AbstractRenderer/AbrstractRenderer.spec.tsx
@@ -1,33 +1,37 @@
 import React from "react";
 import { AbstractRenderer } from "./AbrstractRenderer";
+import { render, screen } from '@testing-library/react';
 
 class AbstractRendererWrapper extends AbstractRenderer {
-    render() {
-        return super.render();
-    }
-    getFormatedText() {
+    getFormatedText(): string {
         return this.props.value;
     }
 }
 
 describe("AbstractRenderer component", () => {
-
     it("should render a pure text", () => {
-        const component = new AbstractRendererWrapper({ value: "TEST", pure: true });
-        expect(component).toBeTruthy();
-        const abstractRendererElement: React.JSX.Element = component.render();
-        expect(abstractRendererElement).toBeTruthy();
-        const fragmentElement = <></>;
-        expect(abstractRendererElement['$$typeof']).toBe(fragmentElement['$$typeof']);
+        render(<AbstractRendererWrapper value="TEST" pure />);
+        const e = screen.getByText("TEST");
+        expect(e).toBeInTheDocument();
+        expect(e.tagName).not.toEqual("SPAN");
+        expect(e.tagName).toEqual("DIV");
+        expect(e).toHaveTextContent("TEST");
+    });
+
+    it("should NOT render a pure text", () => {
+        render(<AbstractRendererWrapper value="TEST" pure={false} />);
+        const e = screen.getByText("TEST");
+        expect(e).toBeInTheDocument();
+        expect(e.tagName).toEqual("SPAN");
+        expect(e).toHaveTextContent("TEST");
     });
 
     it("should render a spanned text", () => {
-        const component = new AbstractRendererWrapper({ value: "TEST" });
-        expect(component).toBeTruthy();
-        const abstractRendererElement: React.JSX.Element = component.render();
-        expect(abstractRendererElement).toBeTruthy();
-        const spanElement: React.ReactElement = <span />;
-        expect(abstractRendererElement['$$typeof']).toBe(spanElement['$$typeof']);
+        render(<AbstractRendererWrapper value="TEST" />);
+        const e = screen.getByText("TEST");
+        expect(e).toBeInTheDocument();
+        expect(e.tagName).toEqual("SPAN");
+        expect(e).toHaveTextContent("TEST");
     });
 
 });
