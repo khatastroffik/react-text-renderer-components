48f714434998dbf668d0e60ac88c325d7495c894
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2025-01-06 13:14:56 +0100

    test: add test cases to AbstractRenderer test suite

--- src/components/AbstractRenderer/AbrstractRenderer.spec.tsx
+++ src/components/AbstractRenderer/AbrstractRenderer.spec.tsx
@@ -27,8 +27,36 @@ describe("AbstractRenderer component", () => {
         expect(e).toHaveTextContent("TEST");
     });
 
-    it("should render a spanned text", () => {
+    it("should render an empty string when value is null", () => {
+        const r = render(<AbstractRendererWrapper value={null} pure />);
+        expect(r.container.innerHTML).toEqual("");
+    });
+
+    it("should render an empty string when value is undefined", () => {
+        const r = render(<AbstractRendererWrapper value={undefined} pure />);
+        expect(r.container.innerHTML).toEqual("");
+    });
+
+    it("should render a spanned empty string when value is null", () => {
+        const r = render(<AbstractRendererWrapper value={null} />);
+        expect(r.container.innerHTML).toEqual("<span></span>");
+    });
+
+    it("should render a spanned string when value is undefined", () => {
+        const r = render(<AbstractRendererWrapper value={undefined} />);
+        expect(r.container.innerHTML).toEqual("<span></span>");
+    });
+
+    it("should render a spanned text when the 'pure' attribute is ommited", () => {
         render(<AbstractRendererWrapper value="TEST" />);
+        const e = screen.getByText("TEST");
+        expect(e).toBeInTheDocument();
+        expect(e.tagName).toEqual("SPAN");
+        expect(e).toHaveTextContent("TEST");
+    });
+
+    it("should render a spanned text when the 'pure' attribute is null", () => {
+        render(<AbstractRendererWrapper value="TEST" pure={null} />);
         const e = screen.getByText("TEST");
         expect(e).toBeInTheDocument();
         expect(e.tagName).toEqual("SPAN");
